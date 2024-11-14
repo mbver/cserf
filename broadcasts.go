fa1fd32321b0cd5842ec9c92e382dfc35793c7e2
phuoc <phuocdongxoai@gmail.com>
2024-11-14 14:36:49 +0700

    remove serf broadcast

--- broadcasts.go
+++ broadcasts.go
@@ -3,14 +3,12 @@ package serf
 import memberlist "github.com/mbver/mlist"
 
 type broadcastManager struct {
-	serfBroadcasts   *memberlist.TransmitCapQueue
 	actionBroadcasts *memberlist.TransmitCapQueue
 	queryBroadcasts  *memberlist.TransmitCapQueue
 }
 
 func newBroadcastManager(numNodes func() int, transmitScale int) *broadcastManager {
 	return &broadcastManager{
-		serfBroadcasts:   memberlist.NewBroadcastQueue(numNodes, transmitScale),
 		actionBroadcasts: memberlist.NewBroadcastQueue(numNodes, transmitScale),
 		queryBroadcasts:  memberlist.NewBroadcastQueue(numNodes, transmitScale),
 	}
@@ -20,11 +18,7 @@ func (m *broadcastManager) GetBroadcasts(overhead, limit int) [][]byte {
 	if limit <= overhead {
 		return nil
 	}
-	msgs := m.serfBroadcasts.GetMessages(overhead, limit)
 	bytesUsed := 0
-	for _, msg := range msgs {
-		bytesUsed += (len(msg) + overhead)
-	}
 
 	queryMsgs := m.queryBroadcasts.GetMessages(overhead, limit-bytesUsed)
 	for _, msg := range queryMsgs {
@@ -32,14 +26,10 @@ func (m *broadcastManager) GetBroadcasts(overhead, limit int) [][]byte {
 	}
 
 	actionMsgs := m.queryBroadcasts.GetMessages(overhead, limit-bytesUsed)
-
+	var msgs [][]byte
 	msgs = append(msgs, queryMsgs...)
 	msgs = append(msgs, actionMsgs...)
 	return msgs
-}
-
-func (m *broadcastManager) broadcastSerf(t msgType, msg interface{}, notify chan struct{}) {
-	m.serfBroadcasts.QueueMsg("", t, msg, notify)
 }
 
 func (m *broadcastManager) broadcastQuery(t msgType, msg msgQuery, notify chan struct{}) {
