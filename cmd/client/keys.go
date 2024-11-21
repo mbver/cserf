e1b5a104d26a53f7a01ee4af9bcd88679f7977fa
phuoc <phuocdongxoai@gmail.com>
2024-11-21 16:22:08 +0700

    key help text

--- cmd/client/keys.go
+++ cmd/client/keys.go
@@ -12,7 +12,9 @@ func isValidKeyCommand(s string) bool {
 
 func KeyCommand() *cobra.Command {
 	return &cobra.Command{
-		Use: "keys <command> <key>",
+		Use:   "keys <command> <key>",
+		Short: "manage encryption keys in serf",
+		Long:  keyHelp,
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) < 1 {
 				return
@@ -39,3 +41,24 @@ func KeyCommand() *cobra.Command {
 		},
 	}
 }
+
+const keyHelp string = `
+Usage: serf keys <command> <key>
+
+  Manage the internal encryption keyring used by Serf. 
+  
+  Modifications made by this command will be BROADCASTED to all 
+  members in the cluster and applied locally on each member.
+  
+  Operations of this command are IDEMPOTENT.
+
+  To facilitate key rotation, Serf allows for multiple encryption keys to be in
+  use simultaneously. Only one key, the "primary" key, will be used for
+  encrypting messages. All other keys are used for decryption only.
+
+  Valid commands are: "install", "use", "remove" and "list". "list" do not
+  require a key.
+  
+  WARNING: Running with multiple encryption keys enabled is recommended as a
+  TRANSITION state only. Performance may be impacted by using multiple keys.
+`
